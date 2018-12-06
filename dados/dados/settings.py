f24d866880b86d585da1e94a96808a569e0f48c7
gabrielgageiro <gabrielgageiro@gmail.com>
2018-12-05 22:54:11 -0200

    m

--- dados/dados/settings.py
+++ dados/dados/settings.py
@@ -67,7 +67,8 @@ ROBOTSTXT_OBEY = True
 #ITEM_PIPELINES = {
 #    'dados.pipelines.DadosPipeline': 300,
 #}
-
+ITEM_PIPELINES = {'dados.pipelines.DadosPipeline': 300,
+                  }
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
