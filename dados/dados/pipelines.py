b3661ecc50395138b00689fd8c8c3b6472dceb81
gabrielgageiro <gabrielgageiro@gmail.com>
2018-12-05 22:54:14 -0200

    m

--- dados/dados/pipelines.py
+++ dados/dados/pipelines.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class DadosPipeline(object):
 
     def open_spider(self, spider):
-      #...
-        pass
+      self.file = open('dados.txt','w')
+
 
     def close_spider(self, spider):
-      pass
+      self.file.close()
 
     def process_item(self, item, spider):
-      #...
+      line = json.dumps(dict(item)) + '\n'
+      self.file.write(line)
       return item
